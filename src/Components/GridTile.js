c6a17cb7945510c8962d109a5a0abdae17509db7
chuacharlie <thchua1300@gmail.com>
2021-03-22 19:07:38 +0800

    Added some basic pages and component to start with

---
+++ src/Components/GridTile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GridTile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default GridTile
