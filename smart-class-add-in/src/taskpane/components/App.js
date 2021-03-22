835347085501842ab479d0b9d05bb4eaf5a70203
Kolivia-c <69305568+Kolivia-c@users.noreply.github.com>
2021-03-22 22:11:07 +0800

    added addin structure

---
+++ smart-class-add-in/src/taskpane/components/App.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { Button, ButtonType } from "office-ui-fabric-react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+/* global Button, console, Header, HeroList, HeroListItem, Office, Progress */
+
+export default class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      listItems: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      listItems: [
+        {
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration"
+        },
+        {
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality"
+        },
+        {
+          icon: "Design",
+          primaryText: "Create and visualize like a pro"
+        }
+      ]
+    });
+  }
+
+  click = async () => {
+    /**
+     * Insert your PowerPoint code here
+     */
+
+
+    Office.context.document.setSelectedDataAsync(
+      "Hello World!",
+      {
+        coercionType: Office.CoercionType.Text
+      },
+      result => {
+        if (result.status === Office.AsyncResultStatus.Failed) {
+          console.error(result.error.message);
+        }
+      }
+    );
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
+      );
+    }
+
+    return (
+      <div className="ms-welcome">
+        <Header logo="assets/logo-filled.png" title={this.props.title} message="Post Questions" />
+        <HeroList message="Type your question here" items={this.state.listItems}>
+          <p className="ms-font-l">
+            Modify the source files, then click <b>Run</b>.
+          </p>
+          <Button
+            className="ms-welcome__action"
+            buttonType={ButtonType.hero}
+            iconProps={{ iconName: "ChevronRight" }}
+            onClick={this.click}
+          >
+            Run
+          </Button>
+        </HeroList>
+        <Header title={this.props.title} message="Students' Questions"/>
+        <HeroList message="There are no questions asked yet!" items={this.state.listItems}/>
+      </div>
+      
+    );
+  }
+}
