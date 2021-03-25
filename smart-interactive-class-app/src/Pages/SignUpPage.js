99e80753e8af2c43217973b169741eea3b4d751e
chuacharlie <thchua1300@gmail.com>
2021-03-26 07:35:10 +0800

    Updated SignUpPage with more inputs

--- smart-interactive-class-app/src/Pages/SignUpPage.js
+++ smart-interactive-class-app/src/Pages/SignUpPage.js
@@ -7,12 +7,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    height: 400,
-    width: 500,
-    backgroundColor: "lightblue",
-    elevation: 0,
-    borderRadius: 20,
+  grid: {
+    height: 500,
+    width: "100%",
   },
   button: {
     background: "#01579b",
@@ -25,8 +22,11 @@ const useStyles = makeStyles((theme) => ({
     padding: "0 30px",
   },
   box: {
-    width: "100%",
-    height: "100%",
+    height: 500,
+    width: 500,
+    backgroundColor: "lightblue",
+    elevation: 0,
+    borderRadius: 20,
   },
   textField: {
     width: "80%",
@@ -38,11 +38,9 @@ const SignUpPage = ({ userType }) => {
   const style = useStyles();
   console.log(userType);
   return (
-    <div className={style.root}>
-      <h1 style={{ textAlign: "center" }}>
-        Sign Up Here
-      </h1>
-      <Paper className={style.paper}>
+    <div>
+      <h2 style={{ textAlign: "center" }}>Join us</h2>
+      <Grid container justify="center" className={style.grid}>
         <Box
           display="flex"
           flexDirection="column"
@@ -51,29 +49,37 @@ const SignUpPage = ({ userType }) => {
           className={style.box}
         >
           <Box width="100%" textAlign="center" py={4}>
-            {(userType === "instructor" ? "Instructor Sign Up" : "Student Sign Up")}
+            {userType === "instructor" ? "Instructor Sign Up" : "Sign Up"}
           </Box>
           <TextField
-            id="instructor-email"
+            id="Name"
+            variant="outlined"
+            placeholder="Name"
+            className={style.textField}
+          />
+          <TextField
+            id="email"
             variant="outlined"
             placeholder="Email"
             className={style.textField}
           />
           <TextField
-            id="instructor-password"
+            id="password"
             variant="outlined"
             placeholder="Password"
             className={style.textField}
           />
-          <Button
-            className={style.button}
-          >
-            Sign up
-          </Button>
+          <TextField
+            id="password"
+            variant="outlined"
+            placeholder="Confirm Password"
+            className={style.textField}
+          />
+          <Button className={style.button}>Sign up</Button>
         </Box>
-      </Paper>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
