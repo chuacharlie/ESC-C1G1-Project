ae18b6b70a7d18f424936e7950af146170f88a5e
chuacharlie <thchua1300@gmail.com>
2021-03-26 02:03:12 +0800

    Edited LoginPage

--- smart-interactive-class-app/src/Pages/LoginPage.js
+++ smart-interactive-class-app/src/Pages/LoginPage.js
@@ -1,13 +1,5 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Box,
-  Container,
-  Grid,
-  Paper,
-  Button,
-  TextField,
-} from "@material-ui/core";
+import { Box, Grid, Paper, Button, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { spacing } from "@material-ui/system";
 
