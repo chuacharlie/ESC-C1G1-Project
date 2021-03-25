42a7146c6d87a6b65bfd60de2959afd6f5c188d4
chuacharlie <thchua1300@gmail.com>
2021-03-26 01:42:50 +0800

    Edited LoginPage and installed material-ui

--- smart-interactive-class-app/src/Components/GridTile.js
+++ smart-interactive-class-app/src/Components/GridTile.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Box, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    height: 400,
+    width: 500,
+  }
+}));
 
 function GridTile() {
+  const style = useStyles();
   return (
-    <div>
-      
-    </div>
+    <Paper className={style.paper} backgroundColor='lightblue' elevation={0}>
+    </Paper>
   )
 }
 
